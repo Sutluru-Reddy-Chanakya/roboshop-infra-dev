ee47bb84d2c34005b6b70e91d6ce90246f74163e
chanakya <you@example.com>
2026-03-26 16:42:37 +0530

    actions

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,4 @@
 module "sg" {
-<<<<<<< HEAD
-  count = length(var.sg_name)
   count = length(var.sg_name)
   source      = "../../terraform-aws-sg"
   project     = var.project
@@ -8,15 +6,3 @@ module "sg" {
   sg_name     = replace(var.sg_name[count.index], "_" , "-")
   vpc_id = local.vpc_id
 }
-
-
-
-=======
-    count = length(var.sg_names)
-    source = "../../terraform-aws-sg"
-    project = var.project
-    environment = var.environment
-    sg_name = replace(var.sg_names[count.index], "_", "-")
-    vpc_id = local.vpc_id
-}
->>>>>>> c67cf2af40dcc899e6f58b76d03ca339caa6d165
