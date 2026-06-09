375f1c250ab917b1fc8b7f2dcfd6db7776f4edca
chanakya <you@example.com>
2026-06-09 15:22:28 +0530

    terraform

--- 40-database/locals.tf
+++ 40-database/locals.tf
@@ -5,7 +5,7 @@ locals {
         Environment = var.environment
         Terraform = "true"
     }
-    # db subnet in 1a AZ
-    db_subnet_id = split(",", data.aws_ssm_parameter.db_subnet_ids.value)[0]
+    # db subnet in 1a AZ - CHANGE THIS LINE
+    db_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]  # Changed from db_subnet_ids to database_subnet_ids
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
