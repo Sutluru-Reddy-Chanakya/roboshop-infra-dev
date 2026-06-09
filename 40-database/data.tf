9a0989b3d05233a1a0bc2ea94f6bc34a8a17276c
chanakya <you@example.com>
2026-06-09 15:28:25 +0530

    terraform

--- 40-database/data.tf
+++ 40-database/data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
-    name = "/${var.project}/${var.environment}/database_subnet_ids"
+    name = "/${var.project}/${var.environment}/db_subnet_ids"
 }
 
 
