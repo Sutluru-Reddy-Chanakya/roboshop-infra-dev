8c839a66a85dbe2bc73b8953a1a729c661e3822b
chanakya <you@example.com>
2026-06-10 16:50:50 +0530

    terraform

--- 40-database/locals.tf
+++ 40-database/locals.tf
@@ -8,4 +8,5 @@ locals {
     # db subnet in 1a AZ - CHANGE THIS LINE
     db_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]  # Changed from db_subnet_ids to database_subnet_ids
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis.value
 }
--- 40-database/var.tf
+++ 40-database/var.tf
@@ -6,4 +6,11 @@ variable "environment" {
     default = "dev"
 }
 
+variable "zone_id" {
+  default = "Z027359523QFZK0V41S4X"
+}
 
+variable "domain_name" {
+    default = "cineniti.in"
+  
+}
