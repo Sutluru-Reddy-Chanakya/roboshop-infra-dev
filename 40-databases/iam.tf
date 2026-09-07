281145e67cc82062bc94f3c3097a8cf391c22bad
chanakya <you@example.com>
2026-09-07 14:35:03 +0530

    terraform

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role" "mysql" {
 
 
 resource "aws_iam_policy" "mysql_policy" {
-  name        = "s3-access-policy"
+  name        = local.mysql_policy_name
   
   description = "A policy to allow EC2 instances to access SSM Parameter Store for MySQL root password"
   policy = file("mysql-iam-policy.json")
