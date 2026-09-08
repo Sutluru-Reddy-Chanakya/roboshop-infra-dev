1b95353091927446c059df445be598500cbeb4a3
chanakya <you@example.com>
2026-09-08 19:31:10 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -8,7 +8,7 @@ dnf install ansible -y
 cd /home/ec2-user
 
 # Use the correct repository (with underscore)
-git clone https://github.com/Sutluru-Reddy-Chanakya/Ansible_Roboshop_roles.git
+git clone https://github.com/Sutluru-Reddy-Chanakya/Ansible-Roboshop-roles-tf.git
 cd Ansible_Roboshop_roles
 git pull
 
