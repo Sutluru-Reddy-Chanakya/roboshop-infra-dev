5dd718c33d25dbf4af06ffd66a678181531174cb
chanakya <you@example.com>
2026-09-08 19:40:30 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,7 +9,8 @@ cd /home/ec2-user
 
 # Use the correct repository (with underscore)
 git clone https://github.com/Sutluru-Reddy-Chanakya/Ansible-Roboshop-roles-tf.git
-cd Ansible_Roboshop_roles
+cd Ansible-Roboshop-roles-tf
+
 git pull
 
 ansible-playbook -e component=$component -e env=$environment roboshop.yml
