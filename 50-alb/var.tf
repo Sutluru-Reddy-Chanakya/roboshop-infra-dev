ae9f5f6af3849e7f354e7a29c3194775f010c94b
chanakya <you@example.com>
2026-09-08 15:28:56 +0530

    terraform

---
+++ 50-alb/var.tf
@@ -0,0 +1,16 @@
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z027359523QFZK0V41S4X"
+}
+
+variable "domain_name" {
+    default = "cineniti.in"
+}
